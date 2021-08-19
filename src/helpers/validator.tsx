663f19b600419bd2f0a6283d874a0afd0a1dab0f
iamsahu <praffulsahu@gmail.com>
2021-08-19 19:36:22 +0530

    Additional properties added

--- src/helpers/validator.tsx
+++ src/helpers/validator.tsx
@@ -20,6 +20,12 @@ export interface Payouts {
 	bpt_tokens?: string;
 	bpt_lp?: string;
 	state?: string;
+	sdt?: string;
+	0x361a5a4993493ce00f61c32d4ecca5512b82ce90?: string;
+	bpt?: string;
+	0x6863bd30c9e313b264657b107352ba246f8af8e0?: string;
+	sdam3crv?: string;
+	0x7d60f21072b585351dfd5e8b17109458d97ec120?: string;
 }
 
 export const schema: JSONSchemaType<Payouts> = {
@@ -35,9 +41,39 @@ export const schema: JSONSchemaType<Payouts> = {
 		bpt_tokens: { type: "string", format: "num", nullable: true },
 		bpt_lp: { type: "string", format: "num", nullable: true },
 		state: { type: "string", nullable: true },
+		sdt: {
+			type: "string",
+			format: "address",
+			nullable: true,
+		},
+		0x361a5a4993493ce00f61c32d4ecca5512b82ce90: {
+			type: "string",
+			format: "num",
+			nullable: true,
+		},
+		bpt: {
+			type: "string",
+			format: "address",
+			nullable: true,
+		},
+		0x6863bd30c9e313b264657b107352ba246f8af8e0: {
+			type: "string",
+			format: "num",
+			nullable: true,
+		},
+		sdam3crv: {
+			type: "string",
+			format: "address",
+			nullable: true,
+		},
+		0x7d60f21072b585351dfd5e8b17109458d97ec120: {
+			type: "string",
+			format: "num",
+			nullable: true,
+		},
 	},
 	required: [],
-	additionalProperties: false,
+	additionalProperties: true,
 };
 
 export const validate = ajv.compile(schema);
